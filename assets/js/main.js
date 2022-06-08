cc06b583fb67bf812ea87376c70ef4f600bb28c5
Dmitry <doesis04@gmail.com>
2022-06-08 14:33:07 +0300

    burger меню, адаптив header + main

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ $(function(){
 	});
 
 	$('.header__burger').click(function(event){
-		$('.header__burger, .header__nav').toggleClass('active')
+		$('.header__burger, .header__nav').toggleClass('active');
+		$('body').toggleClass('lock');
 	})
 })
